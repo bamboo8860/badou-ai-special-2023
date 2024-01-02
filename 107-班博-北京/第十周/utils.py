bf103721cb488a2cd25bff664b7a19abcbe1ac9e
bamboo8860 <148343601+bamboo8860@users.noreply.github.com>
2024-01-02 09:24:09 +0800

    Add files via upload

--- 107-班博-北京/第十周/utils.py
+++ 107-班博-北京/第十周/utils.py
@@ -1,5 +1,6 @@
 import matplotlib.image as mpimg
 import numpy as np
+import cv2
 import tensorflow as tf
 from tensorflow.python.ops import array_ops
 
@@ -13,26 +14,18 @@ def load_image(path):
     crop_img = img[yy: yy + short_edge, xx: xx + short_edge]
     return crop_img
 
-def resize_image(image, size,
-                 method=tf.image.ResizeMethod.BILINEAR,
-                 align_corners=False):
+def resize_image(image, size):
     with tf.name_scope('resize_image'):
-        image = tf.expand_dims(image, 0)
-        image = tf.image.resize_images(image, size,
-                                       method, align_corners)
-        image = tf.reshape(image, tf.stack([-1,size[0], size[1], 3]))
-        return image
+        images = []
+        for i in image:
+            i = cv2.resize(i, size)
+            images.append(i)
+        images = np.array(images)
+        return images
 
-def print_prob(prob, file_path):
-    synset = [l.strip() for l in open(file_path).readlines()]
-    # 将概率从大到小排列的结果的序号存入pred
-    pred = np.argsort(prob)[::-1]
-    # 取最大的1个、5个。
-    top1 = synset[pred[0]]
-    print(("Top1: ", top1, prob[pred[0]]))
-    top5 = [(synset[pred[i]], prob[pred[i]]) for i in range(5)]
-    print(("Top5: ", top5))
-    return top1
-
-
-
+def print_answer(argmax):
+    with open("./data/model/index_word.txt","r",encoding='utf-8') as f:
+        synset = [l.split(";")[1][:-1] for l in f.readlines()]
+        
+    print(synset[argmax])
+    return synset[argmax]
